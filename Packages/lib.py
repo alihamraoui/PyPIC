7dbf8a7acc492d4a13acb04897a3ebc58e8b1e69
hamraoui <ali.hamraoui@etu.u-paris.fr>
2021-09-05 00:12:26 +0200

    Hydrophobic iteraction + parsing arguments + gestion des exceptions

---
+++ Packages/lib.py
@@ -0,0 +1,64 @@
+
+from Bio.PDB.PDBParser import PDBParser
+import numpy as np
+
+def parse_PDB(file):
+    """
+    This function parse pdb file and return Biopython pdb object
+    input : pdb file.
+    output : .
+    """
+    parser=PDBParser()
+    chains =[]
+    structure = parser.get_structure(file,file)
+    model = structure[0]
+    return model 
+
+def calc_dis (atom1,atom2):
+    """
+    This function calculate distance between two atoms
+    input : atoms object of class Bio.pdb.
+    output : Bio.pdb object contain pdb structure.
+    """
+    no_inter_atom = ["CB", "CD", "CD1", "CD2", "CE", "CE1", "CH2",
+                    "CE2", "CE3", "CG", "CG1", "CG2","CZ", "CZ2", "CZ3", "SD"]
+
+    dist = np.linalg.norm(atom1.get_coord() - atom2.get_coord())
+    if (atom1.get_name() in no_inter_atom and
+        atom2.get_name() in no_inter_atom ) :
+        if dist<5 :
+            return dist
+
+
+def hydrophobic(model):
+    """
+    This function calculate hydrophobic interactions
+    input : model = Bio.pdb object contain pdb structure.
+    output : list of residues interacted and distance between them.
+    """
+    amino = {"hydrophobic": [ "ALA", "VAL", "LEU",  "ILE", "MET", "PHE",  "TRP",  "PRO", "TYR"]}
+
+    res_hydrophobic = []
+    for chain in model.get_list():
+        for residue in chain.get_list():
+            if residue.get_resname() in amino['hydrophobic']:
+                res_hydrophobic.append(residue)
+
+    intra = True
+    inter_hydr = []
+    for i, res1 in enumerate(res_hydrophobic):
+        for res2 in res_hydrophobic[i + 1:]:
+            for atom1 in res1:
+                for atom2 in res2:
+                    dist = calc_dis (atom1,atom2)
+                    if dist : 
+                        if intra :
+                            if res1.parent.id == res2.parent.id :
+                              inter_hydr.append([res1.resname, res1.id[1], res1.parent.id, res2.resname, res2.id[1] , res2.parent.id, dist])
+                        else : 
+                            inter_hydr.append([res1.resname, res1.id[1], res1.parent.id, res2.resname, res2.id[1] , res2.parent.id, dist])
+                        break
+                else : 
+                    continue
+                break
+    return inter_hydr
--- PyPIC.py
+++ PyPIC.py
@@ -1,12 +1,16 @@
-from Bio.PDB.PDBParser import PDBParser
-import numpy as np
+from Packages.lib import *
 from tabulate import tabulate
+import argparse
+import sys
 
-def prepare_parser():
-    '''This function parses and return arguments passed in'''
+def parse_args() :
+    '''
+    This function parses and return arguments passed in
+    '''
+
     desc = "Calculating INTRAPROTEIN INTERACTIONS."
 
-    exmp = "Example: PyPIC -PDB 3i40.pdb --hydrophobics "
+    exmp = "Example: python PyPIC -PDB 3i40.pdb --hydrophobics "
 
     parser = argparse.ArgumentParser(prog='PyPIC', description=desc, epilog=exmp)
 
@@ -21,5 +25,28 @@ def prepare_parser():
 
     parser.add_argument('--option', metavar='option', dest='intra', type=str, default="both", choices=['intra', 'inter', 'both'],
                         help='Which type of interactions? (intra, inter, both). e.g : For "inter" chain (e.g. hydrophobics interaction) only interaction between two different chains will be listed. choose reverse.')
+    return parser.parse_args()
+args = parse_args()
 
-    return parser
+def main ():
+    """Start PyPIC......parse options......"""
+    if args.pdbfile[-4:] == '.pdb':
+        try : 
+            model = parse_PDB(args.pdbfile)
+        except : 
+            sys.stderr.write("Error attempting to load : file does not exist :  %s\n" % args.pdbfile )
+            sys.exit(1)
+    else:
+        sys.stderr.write("Error : structure file extension not recognized, it needs to be .pdb \n")
+        sys.exit(1)
+
+    inter_hydr = hydrophobic(model)
+    print('Hydrophobic Interactions within 5 Angstroms')
+    print(tabulate(inter_hydr, headers=['Residue', 'Position', 'Chain','Residue', 'Position', 'Chain', 'Distance (A)'], tablefmt='orgtbl'))
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        sys.stderr.write("User interrupt! \n")
+        sys.exit(0)
