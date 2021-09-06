fffdae49a0df0a325c36575bd21b4fd442a928a2
hamraoui <ali.hamraoui@etu.u-paris.fr>
2021-09-06 15:50:40 +0200

    reporting version 1.0

--- Packages/reporting.py
+++ Packages/reporting.py
@@ -1,4 +1,38 @@
+from datetime import datetime
+import pandas as pd
+
 def reporting (table) :
     """
     This function product html repport with results of interaction
     """
+    #Converting to HTML# saving plot image to local file
+    #image = plot.get_figure().savefig('plot.png')
+
+    #image_tag = '<img src="plot.png">'#writing HTML Content
+
+    infile = '../test/results/' + table
+    df = pd.read_table (infile)
+
+    heading = '<h1> PyPIC Protein Interaction Calculator Report </h1>'
+
+    subheading = '<h3> Hydrophobic interactions </h3>'# Using .now() from datetime library to add Time stamp
+
+    paragraph = '<p> The following residues are considered to participate in interactions if they fall within 5Å range.  \n' + \
+                'ALA, VAL, LEU, ILE, MET, PHE, TRP, PRO, TYR.  \n' + \
+                'Reference: Kyte and Doolittle. </p>'
+
+    now = datetime.now()
+
+    current_time = now.strftime("%m/%d/%Y %H:%M:%S")
+
+    header = '<div class="top">' + heading + subheading +'</div>'
+
+    footer = '<div class="bottom"> <h3> This Report has been Generated on '+ current_time +'</h3> </div>'
+
+    content = paragraph + '<div class="table"> '+df.to_html( index=False)+' </div> \n <div class="chart"> '+ 'image_tag' +'</div>'# Concating everything to a single string
+
+    html = header + content + footer
+
+    outfile = '../test/results/report.html'
+    with open(outfile,'w+') as file:
+        file.write(html)
---
+++ README.md
@@ -0,0 +1,39 @@
+# PyPIC : Protein interaction calculator 
+
+**PyPIC** is a tool designed to calculate non-covalent interactions in a `.pdb` file.
+
+## Dependancies
+- Python 3
+- Biopython
+
+## install Dependancies
+
+```c
+git clone https://github.com/maxibor/protinter.git
+pip install -r requirements.txt
+```
+## How to install PyPIC
+
+```c
+cd PyPIC
+chmod +x PyPIC
+```
+
+## Example
+
+```c
+PyPIC --PDB test/data/3i40.pdb --hydrophobic -csv   
+```
+
+The above example computes the hydrophobic interactions in `3i40.pdb`, displays the results, and writes them in the file `result_hydrophobic.csv`
+
+## List of interactions available
+
+- hydrophobic interactions
+- disulphide bridges
+- ionic interactions
+- aromatic-aromatic interactions
+- aromatic-sulphur interactions
+- cation-pi interactions
+- hydrogen bonds (use results with caution)
+
