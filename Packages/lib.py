f080a94ab695f296a62f8c197ac73ad1d22f2b3f
hamraoui <ali.hamraoui@etu.u-paris.fr>
2021-09-10 00:20:35 +0200

    add hydrogene bonds, fix agrument parsing

--- Packages/lib.py
+++ Packages/lib.py
@@ -1,6 +1,9 @@
 
 from Bio.PDB.PDBParser import PDBParser
 import numpy as np
+from math import degrees
+from tabulate import tabulate
+
 
 def parse_PDB(file):
     """
@@ -13,20 +16,37 @@ def parse_PDB(file):
     model = structure[0]
     return model 
 
-def calc_dis (atom1,atom2):
+
+def output(table,headers,title):
+    tabulate(table, headers, floatfmt=(".2f"), tablefmt="fancy_grid")
+    print(tabulate([],headers=title, tablefmt="pipe"))
+    print(tabulate(table, headers, floatfmt=(".2f"), tablefmt="orgtbl"))
+
+
+def calc_dis (atom1,atom2, d, filter):
     """
     This function calculate distance between two atoms
     input : atoms object of class Bio.pdb.
     output : distance between atoms (Angchtrom).
     """
-    no_inter_atom = ["CB", "CD", "CD1", "CD2", "CE", "CE1", "CH2",
+    if filter:
+        no_inter_atom = ["CB", "CD", "CD1", "CD2", "CE", "CE1", "CH2",
                     "CE2", "CE3", "CG", "CG1", "CG2","CZ", "CZ2", "CZ3", "SD"]
+        if (atom1.get_name() in no_inter_atom and
+            atom2.get_name() in no_inter_atom ) :
+            dist = np.linalg.norm(atom1.get_coord() - atom2.get_coord())
+            if dist< d : return dist
+    else :
+        dist = np.linalg.norm(atom1.get_coord() - atom2.get_coord())
+        if dist< d : return dist
 
-    dist = np.linalg.norm(atom1.get_coord() - atom2.get_coord())
-    if (atom1.get_name() in no_inter_atom and
-        atom2.get_name() in no_inter_atom ) :
-        if dist<5 :
-            return dist
+
+
+def get_angle(atom1, H, atom2):
+    vecA = atom1.coord - H.coord
+    vecB = atom2.coord - H.coord
+    angle = np.arccos(np.dot(vecA,vecB) / (np.linalg.norm(vecA) * np.linalg.norm(vecB)))
+    return degrees(angle)
 
 
 def hydrophobic(model):
@@ -49,11 +69,11 @@ def hydrophobic(model):
         for res2 in res_hydrophobic[i + 1:]:
             for atom1 in res1:
                 for atom2 in res2:
-                    dist = calc_dis (atom1,atom2)
+                    dist = calc_dis (atom1,atom2,5, filter=True)
                     if dist : 
                         if intra :
-                            if res1.parent.id == res2.parent.id :
-                              inter_hydr.append([res1.resname, res1.id[1], res1.parent.id, res2.resname, res2.id[1] , res2.parent.id, dist])
+                            if res1.parent.id == res2.parent.id:
+                                inter_hydr.append([res1.resname, res1.id[1], res1.parent.id, res2.resname, res2.id[1] , res2.parent.id, dist])
                         else : 
                             inter_hydr.append([res1.resname, res1.id[1], res1.parent.id, res2.resname, res2.id[1] , res2.parent.id, dist])
                         break
@@ -61,3 +81,33 @@ def hydrophobic(model):
                     continue
                 break
     return inter_hydr
+
+
+def main_main_hydrogene(model) :
+    """
+    This function calculate hydrogene bonds
+    input : model = Bio.pdb object contain pdb structure.
+    output : list of donor and accepto, distance between them and angle between donor-H-acceptor.
+    """
+    mm_hydrogenes = []
+    for chain in model.get_list():
+        res_list = chain.get_list()
+        for res1 in res_list:
+            for res2 in res_list:
+                if (res1.parent.id == res2.parent.id 
+                    and res1.resname != 'HOH' and res2.resname != 'HOH'):
+                    for donor in res1:
+                        if donor.name == 'N':
+                            for accept in res2 :
+                                if accept.name == 'O':
+                                    dist = calc_dis (donor, accept, 3.5, filter = False)
+                                    if abs(res1.id[1] - res2.id[1]) >= 2 :
+                                        if dist :
+                                            try :
+                                                donor_H = donor.parent['H']
+                                                Dh = donor_H - accept
+                                                ang = get_angle(donor, donor_H, accept)
+                                                mm_hydrogenes.append([res1.resname, res1.id[1], donor.id ,res1.parent.id, res2.resname, res2.id[1] ,\
+                                                                accept.id , res2.parent.id, dist, Dh, ang],)
+                                            except : pass
+    return mm_hydrogenes
--- PyPIC.py
+++ PyPIC.py
@@ -1,6 +1,4 @@
-from numpy.lib.shape_base import tile
 from Packages.lib import *
-from tabulate import tabulate
 import argparse
 import sys
 
@@ -9,28 +7,40 @@ def parse_args() :
     This function parses and return arguments passed in
     '''
 
-    desc = "Calculating INTRAPROTEIN INTERACTIONS."
+    desc = "PYPIC : CALCULATING INTRAPROTEIN INTERACTIONS."
 
-    exmp = "Example: python PyPIC -PDB 3i40.pdb --hydrophobics "
+    exmp = "Example: python PyPIC -PDB 3i40.pdb -hydrophobics "
 
     parser = argparse.ArgumentParser(prog='PyPIC', description=desc, epilog=exmp)
 
-    parser.add_argument('--PDB', metavar='File.pdb', dest='pdbfile', required=True,
+    parser.add_argument('-pdb', metavar='File.pdb', dest='pdbfile', required=True,
                         help='An PDB file.')
 
-    parser.add_argument('--hydrophobics', metavar='H', dest='hydrophobic', type=str,
+    parser.add_argument('-hydrophob', dest='hydrophob', action='store_true',
                         help='Checking for Hydrophobic Interaction')
 
-    parser.add_argument('--disulphide', metavar='Disulphide-Bridges', dest='disulph', type=str,
+    parser.add_argument('-disulphide', metavar='', dest='disulph', type=str,
                         help='Checking for Disulphide Bridges')
 
-    parser.add_argument('--option', metavar='option', dest='intra', type=str, default="both", choices=['intra', 'inter', 'both'],
-                        help='Which type of interactions? (intra, inter, both). e.g : For "inter" chain (e.g. hydrophobics interaction) only interaction between two different chains will be listed. choose reverse.')
-    return parser.parse_args()
-args = parse_args()
+    parser.add_argument('-mmh', dest='mmhydrogenes', action='store_true',
+                        help='Checking for main main Hydrogen bond')
 
-def main ():
-    """Start PyPIC......parse options......"""
+    parser.add_argument('-tsv', metavar='', dest='tsv', type=str,
+                        help='Export the result tables in tsv format')
+
+    parser.add_argument('-option', metavar='option', dest='intra', type=str, default="both", choices=['intra', 'inter', 'both'],
+                        help='Which type of interactions? (intra, inter, both). e.g : For "inter" chain (e.g. hydrophobics interaction) only interaction between two different chains will be listed. choose reverse.')
+    
+    parser.add_argument('--version', action='version', version='%(prog)s 1.0')
+    
+    return parser.parse_args()
+
+
+def main (): 
+    """
+    Start PyPIC......parse options......
+    """
+    args = parse_args()
     if args.pdbfile[-4:] == '.pdb':
         try : 
             model = parse_PDB(args.pdbfile)
@@ -41,35 +51,31 @@ def main ():
         sys.stderr.write("Error : structure file extension not recognized, it needs to be .pdb \n")
         sys.exit(1)
 
-    table_hydrophobic = hydrophobic(model)
-    headers=['Residue', 'Position', 'Chain','Residue', 'Position', 'Chain', 'Distance (A)']
-    title = ['\n ' + '\033[1m' + 'Hydrophobic Interactions within 5 Angstroms' + '\033[0m']
-    table = tabulate(table_hydrophobic, headers, floatfmt=(".2f"), tablefmt="fancy_grid")
+    if args.hydrophob :
+        table= hydrophobic(model)
+        headers=['Residue', 'Position', 'Chain','Residue', 'Position', 'Chain', 'Distance (A)']
+        title = ['\n ' + '\033[1m' + 'Hydrophobic Interactions within 5 Angstroms' + '\033[0m']
+        output(table,headers,title)
 
+    if args.mmhydrogenes:
+        table= main_main_hydrogene( model)
+        headers=['Residue', 'Position', 'donor', 'Chain','Residue', 'Position','acceptor', 'Chain', 'd(don-acc)', 'd(Hdon-acc)', 'agnle(don-H-acc)']
+        title = ['\n ' + '\033[1m' + 'Main-Main chain Hydrogene bonds' + '\033[0m']
+        output(table,headers,title)
 
-    """ Output """
-
-    print(tabulate([],headers=title, tablefmt="pipe"))
-    print(table)
-
-    """ """
-
-    """ Repporting """
-
-    #reporting(table)
-
-    """ """
-
-
-    tsv = True
-    if tsv : 
+    if args.tsv: 
         try : 
-            tsv_table = tabulate(table_hydrophobic, headers, floatfmt=(".2f"), tablefmt="tsv")
+            tsv_table = tabulate(table, headers, floatfmt=(".2f"), tablefmt="tsv")
             text_file=open("test/results/table.tsv","w")
             text_file.write(tsv_table)
             text_file.close()
         except : 
-            sys.stderr.write("Error in writing tsv file")
+            sys.stderr.write("Error in writing tsv file  \n")
+
+    """ Repporting """
+    #reporting(table)
+    """ """
+
 
 if __name__ == '__main__':
     try:
